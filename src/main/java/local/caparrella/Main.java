4bc7513db7f137df2ddca2a5deb9d0323a990d66
jordi <jraichs@inscaparrella.cat>
2023-12-12 12:03:11 +0100

    versió inicial

---
+++ src/main/java/local/caparrella/Main.java
@@ -0,0 +1,18 @@
+package local.caparrella;
+
+public class Main {
+    public static void main(String[] args) {
+        // Crea un array d'objectes Persona
+        Persona[] persones = new Persona[3];
+
+        // Inicialitza els objectes a l'array
+        persones[0] = new Persona("Joan", 25);
+        persones[1] = new Persona("Maria", 30);
+        persones[2] = new Persona("Carles", 22);
+
+        // Accedeix als objectes a l'array
+        for (Persona persona : persones) {
+            System.out.println("Nom: " + persona.getNom() + ", Edat: " + persona.getEdat());
+        }
+    }
+}
