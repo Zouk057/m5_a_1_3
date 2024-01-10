65ebdc5135252ee5bde4271f4856badf47ecbd6f
jordi <jraichs@inscaparrella.cat>
2024-01-10 19:42:35 +0100

    final projecte

--- src/main/java/local/caparrella/Main.java
+++ src/main/java/local/caparrella/Main.java
@@ -3,12 +3,13 @@ package local.caparrella;
 public class Main {
     public static void main(String[] args) {
         // Crea un array d'objectes Persona
-        Persona[] persones = new Persona[3];
+        Persona[] persones = new Persona[4];
 
         // Inicialitza els objectes a l'array
         persones[0] = new Persona("Joan", 25);
         persones[1] = new Persona("Maria", 30);
         persones[2] = new Persona("Pepe", 40);
+        persones[3] = new Persona("Maria", 33);
       
 
         // Accedeix als objectes a l'array
